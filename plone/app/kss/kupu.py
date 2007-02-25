db452a7ce59bc1da2c16ecfcf6895e680576257e
Jeroen Vloothuis <jeroen@vloothuis.me>
2007-02-25 16:32:27 +0000

    Removed blink tag etc. in kupu save action
    svn path=/plone.app.kss/trunk/; revision=13145

--- plone/app/kss/kupu.py
+++ plone/app/kss/kupu.py
@@ -4,8 +4,6 @@ from azaxview import AzaxBaseView
 from kss.core import force_unicode, kssaction
 from interfaces import IPloneAzaxView
 
-from datetime import datetime
-
 class KupuSaveView(AzaxBaseView):
 
     # --
@@ -17,11 +15,9 @@ class KupuSaveView(AzaxBaseView):
     @kssaction
     def save(self, text, fieldname):
         "In-place saving of kupu text area's."
-        time = datetime.now()
         corecommands = self.getCommandSet('core')
         self.context.getField(fieldname).set(self.context, text, mimetype='text/html')
-        messageid = "kupu-save-message-%s" % fieldname
-        selector = corecommands.getCssSelector('#kupu-editor-%s div.kupu-tb' % fieldname)
-        corecommands.deleteNode('#' + messageid)
+        
+        selector = corecommands.getCssSelector('body')
         corecommands.insertHTMLAsLastChild(
-            selector, '<blink id="%s" style="color: red; font-weight: bold; font-size: 16pt">Document saved: %s</blink>' % (messageid, time.strftime('%H:%M:%S')))
+            selector, '<div class="kupu-save-message">Document saved</div>')
