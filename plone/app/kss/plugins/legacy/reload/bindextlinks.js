3507bc0b43d5c904e30f781678486c51716d00e5
Godefroid Chapelle <gotcha@bubblenet.be>
2007-07-22 15:50:22 +0000

    external links is not always loaded anymore
    svn path=/plone.app.kss/trunk/; revision=16137

--- plone/app/kss/plugins/legacy/reload/bindextlinks.js
+++ plone/app/kss/plugins/legacy/reload/bindextlinks.js
@@ -1,11 +1,23 @@
 
 // bind external links marking on load
 
+kukit.plonelegacy = {};
+
+if (typeof(scanforlinks) == 'undefined') {
+    kukit.plonelegacy.bindExternalLinks = function() {}
+    }
+else {
+    kukit.plonelegacy.bindExternalLinks = function() {
+        scanforlinks();
+        }
+    }
 
 kukit.actionsGlobalRegistry.register("bindExternalLinks", function (oper) {
-        scanforlinks();
+        kukit.plonelegacy.bindExternalLinks();
         kukit.logDebug('Plone external links marker registered');
     });
 
 kukit.log('actions for Plone legacy js registered (mark external links)');
+
+
 
