567af1849915ec56aadcdbeacb7bcd0f18b7e102
Simone Deponti <shywolf9982@gmail.com>
2008-03-27 09:13:55 +0000

    First revision of setup base class
    svn path=/plone.app.kss/branches/1.4-kss-test/; revision=20074

---
+++ plone/app/kss/tests/setupbase.py
@@ -0,0 +1,43 @@
+from Products.Five.browser import BrowserView
+from Acquisition import aq_parent
+from OFS.interfaces import IApplication
+
+class SetUpBase(BrowserView):
+    """This class provides the basic bootstrap set for every
+    test suite bootstrapping. You must inherit from this class
+    and override just the run method"""
+
+    _required_role = 'Manager'
+
+    def __init__(self, context, request):
+        super(BootstrapBase, self).__init__(context, request)
+        #go up to the root
+        parent = aq_parent(self.context.aq_inner)
+        while not IApplication.providedBy(parent):
+            try:
+                parent = parent.aq_parent
+            except AttributeError:
+                break
+        self.zoperoot = parent
+
+    def checkPermission(self):
+        method = self.request.get("REQUEST_METHOD", "GET").upper()
+        userid = self.request.get("userid", None)
+        password = self.request.get("password", None)
+        if method == "POST" and userid is not None and password is not None:
+            admin = self.zoperoot.acl_users.authenticate(userid, password, None)
+            if self._required_role in admin.getRoles():
+                return True
+        return False
+    
+    def run(self):
+        """Just override me"""
+        raise Excpetion("Functionality not implemented")
+
+    def start(self):
+        if self.checkPermission():
+            return self.run()
+        else:
+            raise Exception("You are trying to run potentially disruptive code without providing a good auth")
+
+    __call__ = start
