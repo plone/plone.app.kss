cb838072864a9d1cdb2e6f5ae78139b667f5e5de
David Glick <dglick@gmail.com>
2012-05-18 13:05:53 -0400

    remove hard dependency on Archetypes

--- plone/app/kss/__init__.py
+++ plone/app/kss/__init__.py
@@ -1,6 +1,4 @@
-from Products.Archetypes.public import process_types, listTypes
-from Products.CMFCore import utils
-from config import SKINS_DIR, GLOBALS, PROJECTNAME, ADD_CONTENT_PERM
+from config import SKINS_DIR, GLOBALS
 from Products.CMFCore.DirectoryView import registerDirectory, registerFileExtension
 from Products.CMFCore.FSFile import FSFile
 
@@ -13,15 +11,3 @@ def initialize(context):
 
     # Register kss extension to allow it used from fs skins
     registerFileExtension('kss', FSFile)
-
-    # content initialization
-    content_types, constructors, ftis = process_types(
-        listTypes(PROJECTNAME),
-        PROJECTNAME)
-
-    utils.ContentInit(
-        PROJECTNAME + ' Content',
-        content_types = content_types,
-        permission = ADD_CONTENT_PERM,
-        extra_constructors = constructors,
-        ).initialize(context)
